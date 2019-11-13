250521fc615bf7abf342feb47d73e5e47dfc1c76
Parradizze <theo.boivin1@viacesi.fr>
2019-11-13 22:51:24 +0100

    campus

--- site/resources/views/campus.blade.php
+++ site/resources/views/campus.blade.php
@@ -17,8 +17,25 @@
     <div class="card my-4 text-center">
         <div class="card-body text-center">
             <h2 class="card-title">Carte des implémentations</h2>
-            <img src="assets/carte.png" alt="carte" class="w-40 img-fluid">
-        </div>
+            <div class="card-body text-left">
+                <h5>CESI c'est <strong>25 campus</strong>, répartis sur toute le territoire français, pour :</h5>
+                <ul>
+                    <li>Être proche des entreprises et des salariés partout en France.</li>
+                    <li>Créer des liens forts avec les sociétés, les organisations et les institutions locales et nationales.</li>
+                    <li>Suivre un cursus sur-mesure, en mobilité, n’importe où en France.</li>
+                </ul>
+            </div>
+            <div class="box">
+                <div class="card-body">
+                <img src="assets/carte.png" alt="carte" class="w-40 img-fluid">
+                </div>
+                <div class="card-body col-md-4">
+                    <h1 class="card-title">+ de 1500 formations & modules sont enseignés sur nos 25 campus</h1>
+                </div>
+            </div>
     </div>
+    </div>
+
+
 
 @endsection
